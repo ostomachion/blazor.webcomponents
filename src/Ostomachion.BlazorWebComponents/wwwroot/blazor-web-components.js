a7be865a9f5b35c8907dc673d2326581b3f7feca
josh@hufford.io <josh@hufford.io>
2023-02-17 12:13:06 -0500

    JS cleanup

--- src/Ostomachion.BlazorWebComponents/wwwroot/blazor-web-components.js
+++ src/Ostomachion.BlazorWebComponents/wwwroot/blazor-web-components.js
@@ -3,12 +3,15 @@
         customElements.define(elementName, class extends HTMLElement {
             constructor() {
                 super()
-                    .attachShadow({ mode: 'open' });
-
-                const template = document.createElement('template');
-                template.innerHTML = (templateCss ? `<style>\n${templateCss}\n</style>\n` : '') + templateHtml;
-                this.shadowRoot.append(...template.content.childNodes);
+                    .attachShadow({ mode: 'open' })
+                    .append(...getTemplateNodes(templateHtml, templateCss));
             }
         });
+    }
+
+    function getTemplateNodes(templateHtml, templateCss) {
+        const template = document.createElement('template');
+        template.innerHTML = (templateCss ? `<style>\n${templateCss}\n</style>\n` : '') + templateHtml;
+        return template.content.childNodes;
     }
 }
