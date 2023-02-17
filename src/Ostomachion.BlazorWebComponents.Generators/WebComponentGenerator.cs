046caad9426df4d3cc6f51d766ee5f6c0da33800
josh@hufford.io <josh@hufford.io>
2023-02-16 19:47:57 -0500

    Remove comments

--- src/Ostomachion.BlazorWebComponents.Generators/WebComponentGenerator.cs
+++ src/Ostomachion.BlazorWebComponents.Generators/WebComponentGenerator.cs
@@ -89,19 +89,12 @@ public class WebComponentGenerator : IIncrementalGenerator
         }
     }
 
-    // determine the namespace the class/enum/struct is declared in, if any
     private static string GetNamespace(BaseTypeDeclarationSyntax syntax)
     {
-        // If we don't have a namespace at all we'll return an empty string
-        // This accounts for the "default namespace" case
         var value = string.Empty;
 
-        // Get the containing syntax node for the type declaration
-        // (could be a nested type, for example)
         SyntaxNode? potentialNamespaceParent = syntax.Parent;
 
-        // Keep moving "out" of nested classes etc until we get to a namespace
-        // or until we run out of parents
         while (potentialNamespaceParent is not null
             and not NamespaceDeclarationSyntax
             and not FileScopedNamespaceDeclarationSyntax)
@@ -109,14 +102,10 @@ public class WebComponentGenerator : IIncrementalGenerator
             potentialNamespaceParent = potentialNamespaceParent.Parent;
         }
 
-        // Build up the final namespace by looping until we no longer have a namespace declaration
         if (potentialNamespaceParent is BaseNamespaceDeclarationSyntax namespaceParent)
         {
-            // We have a namespace. Use that as the type
             value = namespaceParent.Name.ToString();
 
-            // Keep moving "out" of the namespace declarations until we 
-            // run out of nested namespace declarations
             while (true)
             {
                 if (namespaceParent.Parent is not NamespaceDeclarationSyntax parent)
@@ -124,13 +113,11 @@ public class WebComponentGenerator : IIncrementalGenerator
                     break;
                 }
 
-                // Add the outer namespace as a prefix to the final namespace
                 value = $"{namespaceParent.Name}.{value}";
                 namespaceParent = parent;
             }
         }
 
-        // return the final namespace
         return value;
     }
 }
