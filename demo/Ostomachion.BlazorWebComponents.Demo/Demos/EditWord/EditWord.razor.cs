6f72254a1e5a8a393193c6c5628cf15335fd97fc
josh@hufford.io <josh@hufford.io>
2023-02-28 12:32:49 -0500

    Implement more functionality on EditWord

--- demo/Ostomachion.BlazorWebComponents.Demo/Demos/EditWord/EditWord.razor.cs
+++ demo/Ostomachion.BlazorWebComponents.Demo/Demos/EditWord/EditWord.razor.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 
 namespace Ostomachion.BlazorWebComponents.Demo.Demos.EditWord;
 
@@ -8,4 +9,26 @@ public partial class EditWord
     [Parameter]
     [EditorRequired]
     public string Value { get; set; } = null!;
+
+    public bool Editing { get; set; }
+
+    private ElementReference _input;
+
+    public void Click(MouseEventArgs e)
+    {
+        Editing = true;
+    }
+
+    public void Blur(FocusEventArgs e)
+    {
+        Editing = false;
+    }
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (Editing)
+        {
+            await _input.FocusAsync();
+        }
+    }
 }
