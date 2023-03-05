d7f59d53a7fa871020913adc2148f660062cffe8
josh@hufford.io <josh@hufford.io>
2023-03-05 13:21:48 -0500

    Set up IJSComponentConfiguration service

--- demo/Ostomachion.BlazorWebComponents.Demo/Program.cs
+++ demo/Ostomachion.BlazorWebComponents.Demo/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Ostomachion.BlazorWebComponents;
 using Ostomachion.BlazorWebComponents.Demo;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +8,6 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddSingleton<IJSComponentConfiguration>(sp => builder.RootComponents);
 
 builder.Build().RunAsync();
