1eb8ced16080d9db54c67d99337baa9194724145
josh@hufford.io <josh@hufford.io>
2023-03-06 12:52:15 -0500

    Implement editable-list demo

---
+++ demo/Ostomachion.BlazorWebComponents.Demo/Demos/EditableList/EditableList.razor.cs
@@ -0,0 +1,31 @@
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Ostomachion.BlazorWebComponents.Demo.Models;
+
+namespace Ostomachion.BlazorWebComponents.Demo.Demos.EditableList;
+
+[WebComponent("editable-list")]
+public partial class EditableList
+{
+    [Parameter]
+    [EditorRequired]
+    public string Title { get; set; } = default!;
+
+    [Parameter]
+    [EditorRequired]
+    public List<ListItem> ListItems { get; set; } = default!;
+
+    [Parameter]
+    [EditorRequired]
+    public string AddItemText { get; set; } = default!;
+
+    public string InputValue { get; set; } = default!;
+
+    private void RemoveListItem(ListItem listItem) => ListItems.Remove(listItem);
+
+    private void AddListItem(MouseEventArgs e)
+    {
+        ListItems.Add(InputValue);
+        InputValue = String.Empty;
+    }
+}
