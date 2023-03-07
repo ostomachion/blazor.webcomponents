fbb3b44c228a215d4d68351fb8d20765aa1bc058
josh@hufford.io <josh@hufford.io>
2023-03-06 21:20:56 -0500

    Doing some tests

--- src/Ostomachion.BlazorWebComponents.Generators/WebComponentGenerator.cs
+++ src/Ostomachion.BlazorWebComponents.Generators/WebComponentGenerator.cs
@@ -9,264 +9,31 @@ namespace Ostomachion.BlazorWebComponents.Generators;
 [Generator(LanguageNames.CSharp)]
 public partial class WebComponentGenerator : IIncrementalGenerator
 {
-    // TODO: Create copies of .css files in wwwroot
     public void Initialize(IncrementalGeneratorInitializationContext context)
     {
-        // Do a simple filter for classes.
-        var webComponentClasses = context.SyntaxProvider
-            .CreateSyntaxProvider(
-                predicate: static (s, _) => IsSyntaxTargetForGeneration(s),
-                transform: static (ctx, _) => GetSemanticTargetForGeneration(ctx))
-            .Where(static m => m is not null)!;
+        // Get all .razor files.
+        var files = context.AdditionalTextsProvider
+            .Select((text, _) => text.Path)
+            .Where(path => Path.GetExtension(path) == ".razor");
 
-        // Combine the selected classes with the `Compilation`.
-        var compilationAndClasses = context.CompilationProvider.Combine(webComponentClasses.Collect());
+        // Get all classes that inherit WebComponentBase.
+        var webComponents = context.SyntaxProvider.CreateSyntaxProvider(
+            (n, _) => n is ClassDeclarationSyntax c &&
+                c.BaseList is not null &&
+                c.BaseList.Types.Any(t => t.Type is NameSyntax),
+            (context, _) =>
+            {
+                var symbol = (INamedTypeSymbol)context.SemanticModel.GetDeclaredSymbol(context.Node)!;
+                return (context.Node.SyntaxTree.FilePath, symbol.Name, symbol.ContainingNamespace, symbol.BaseType);
+            })
+            .Collect();
 
-        // Generate the source using the compilation and classes.
-        context.RegisterSourceOutput(compilationAndClasses,
-            static (context, source) => GenerateSource(context, source.Right!));
-    }
+        //var files = context.SyntaxProvider.CreateSyntaxProvider(
+        //    static (n, _) => n is 
 
-    private static bool IsSyntaxTargetForGeneration(SyntaxNode node) => node is ClassDeclarationSyntax { AttributeLists.Count: > 0 };
-
-    private static WebComponentSyntax? GetSemanticTargetForGeneration(GeneratorSyntaxContext context)
-    {
-        var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
-        var attributeSyntax = GetAttribute("Ostomachion.BlazorWebComponents.WebComponentAttribute", context, classDeclarationSyntax.AttributeLists);
-        var slotSyntaxes = GetSlotProperties(context);
-        return attributeSyntax is null ? null : new WebComponentSyntax(classDeclarationSyntax, attributeSyntax, slotSyntaxes);
-    }
-
-    private static IEnumerable<SlotSyntax> GetSlotProperties(GeneratorSyntaxContext context)
-    {
-        Dictionary<string, SlotSyntax> value = new();
-        HashSet<string> templates = new();
-        var classDeclarationSyntax = (ClassDeclarationSyntax)context.Node;
-        var properties = classDeclarationSyntax.Members.OfType<PropertyDeclarationSyntax>();
-        foreach (var property in properties)
+        context.RegisterImplementationSourceOutput(webComponents, (context, webComponents) =>
         {
-            var name = property.Identifier.ToString();
-            var attribute = GetAttribute("Ostomachion.BlazorWebComponents.SlotAttribute", context, property.AttributeLists);
-            if (attribute is not null)
-            {
-                string typeString;
-                var symbol = context.SemanticModel.GetTypeInfo(property.Type).Type;
-                typeString = symbol!.ToDisplayString();
-
-                value.Add(name, new SlotSyntax(property, attribute, typeString));
-            }
-
-            if (name.EndsWith("Template"))
-            {
-                var baseName = name.Substring(0, name.Length - "Template".Length);
-                _ = templates.Add(baseName);
-            }
-        }
-
-        foreach (var baseName in templates)
-        {
-            if (value.TryGetValue(baseName, out var slotSyntax))
-            {
-                slotSyntax.IsTemplated = true;
-            }
-        }
-
-        return value.Values;
-    }
-
-    private static AttributeSyntax? GetAttribute(string fullName, GeneratorSyntaxContext context, SyntaxList<AttributeListSyntax> attributeLists)
-    {
-        foreach (var attributeListSyntax in attributeLists)
-        {
-            foreach (var attributeSyntax in attributeListSyntax.Attributes)
-            {
-                if (context.SemanticModel.GetSymbolInfo(attributeSyntax).Symbol is not IMethodSymbol attributeSymbol)
-                {
-                    continue;
-                }
-
-                if (attributeSymbol.ContainingType.ToDisplayString() == fullName)
-                {
-                    return attributeSyntax;
-                }
-            }
-        }
-
-        return null;
-    }
-
-    private static void GenerateSource(SourceProductionContext context, ImmutableArray<WebComponentSyntax> webComponentSyntaxes)
-    {
-        foreach (var item in webComponentSyntaxes)
-        {
-            var className = item.ClassDeclarationSyntax.Identifier.Text;
-
-            var namespaceName = GetNamespace(item.ClassDeclarationSyntax);
-
-            var styleSheetUrl = GetStyleSheetUrl(item.ClassDeclarationSyntax.SyntaxTree.FilePath);
-            
-            var builder = new StringBuilder()
-                .AppendLine($$"""
-                    #nullable enable
-
-                    using System;
-                    using Microsoft.AspNetCore.Components;
-                    using Microsoft.AspNetCore.Components.Rendering;
-                    using Ostomachion.BlazorWebComponents;
-
-                    namespace {{namespaceName}};
-
-                    partial class {{className}} : IWebComponent
-                    {
-                        private static string? _identifier;
-                        public static string? Identifier
-                        {
-                            get => _identifier;
-                            set
-                            {
-                                if (_identifier is not null)
-                                {
-                                    throw new InvalidOperationException("Identifier has already been set.");
-                                }
-                                _identifier = value;
-                            }
-                        }
-                    
-                        public static string? StylesheetUrl => {{(styleSheetUrl is null ? "null;" : $$"""_identifier is null ? null : $"{{styleSheetUrl}}";""")}}
-                    """);
-
-            if (item.SlotSyntaxes.Any())
-            {
-                _ = builder.Append("""
-
-                        protected override void BuildRenderTreeSlots(RenderTreeBuilder builder)
-                        {
-                    """);
-
-                var sequence = 0;
-                foreach (var slotSyntax in item.SlotSyntaxes)
-                {
-                    _ = builder.AppendLine();
-
-                    var type = slotSyntax.TypeString;
-                    var name = slotSyntax.PropertyDeclarationSyntax.Identifier.ValueText;
-
-                    // TODO: Support optional names. Default to property name.
-                    var slotName = slotSyntax.AttributeSyntax.ArgumentList!.Arguments.First().GetText();
-
-                    var rootElementName = slotSyntax.AttributeSyntax.ArgumentList!.Arguments
-                        .FirstOrDefault(x => x.NormalizeWhitespace().GetText().ToString().StartsWith("RootElement = "))
-                        ?.NormalizeWhitespace().GetText().ToString().Substring("RootElement = ".Length);
-
-                    rootElementName ??= slotSyntax.IsTemplated || type == "Microsoft.AspNetCore.Components.RenderFragment" ? "\"div\"" : "\"span\"";
-
-                    _ = builder.AppendLine($$"""
-                                if ((object?){{name}} is not null && RenderedSlots.Contains("{{name}}"))
-                                {
-                                    builder.OpenElement({{sequence++}}, {{rootElementName}});
-                                    builder.AddAttribute({{sequence++}}, "slot", {{slotName}});
-
-                                    builder.AddAttribute({{sequence++}}, "xmlns:wc", GetType().Namespace);
-                                    builder.AddAttribute({{sequence++}}, $"wc:{nameof({{name}})}");
-                        """);
-
-                    if (slotSyntax.IsTemplated)
-                    {
-                        _ = builder.AppendLine($$"""
-                                        builder.AddAttribute({{sequence++}}, $"wc:{nameof({{name}}Template)}");
-
-                                        if (this.{{name}}Template is null)
-                                        {
-                                            builder.AddContent({{sequence++}}, this.{{name}});
-                                        }
-                                        else
-                                        {
-                                            builder.AddContent({{sequence++}}, this.{{name}}Template, this.{{name}});
-                                        }
-                            """);
-                    }
-                    else
-                    {
-                        _ = builder.AppendLine($$"""
-                                        builder.AddContent({{sequence++}}, this.{{name}});
-                            """);
-                    }
-
-                    _ = builder.AppendLine($$"""
-                                    builder.CloseElement();
-                                }
-                        """);
-                }
-
-                _ = builder.AppendLine($$"""
-                        }
-                    """);
-            }
-
-            foreach (var slotSyntax in item.SlotSyntaxes)
-            {
-                var name = slotSyntax.PropertyDeclarationSyntax.Identifier.ValueText;
-
-                // TODO: Support optional names. Default to property name.
-                var slotName = slotSyntax.AttributeSyntax.ArgumentList!.Arguments.First().GetText();
-
-                _ = builder.AppendLine($$"""
-
-                        private RenderFragment {{name}}Slot => (builder) =>
-                        {
-                            RenderedSlots.Add("{{name}}");
-                            builder.OpenElement(0, "slot");
-                            builder.AddAttribute(1, "name", {{slotName}});
-                            builder.AddContent(2, {{slotName}});
-                            builder.CloseElement();
-                        };
-                    """);
-            }
-
-            _ = builder.Append("}");
-
-            context.AddSource($"{className}.g.cs", builder.ToString());
-        }
-    }
-
-    private static string GetNamespace(BaseTypeDeclarationSyntax syntax)
-    {
-        var value = string.Empty;
-
-        SyntaxNode? potentialNamespaceParent = syntax.Parent;
-
-        while (potentialNamespaceParent is not null
-            and not NamespaceDeclarationSyntax
-            and not FileScopedNamespaceDeclarationSyntax)
-        {
-            potentialNamespaceParent = potentialNamespaceParent.Parent;
-        }
-
-        if (potentialNamespaceParent is BaseNamespaceDeclarationSyntax namespaceParent)
-        {
-            value = namespaceParent.Name.ToString();
-
-            while (true)
-            {
-                if (namespaceParent.Parent is not NamespaceDeclarationSyntax parent)
-                {
-                    break;
-                }
-
-                value = $"{namespaceParent.Name}.{value}";
-                namespaceParent = parent;
-            }
-        }
-
-        return value;
-    }
-
-    private static string? GetStyleSheetUrl(string csPath)
-    {
-        // TODO: I really want to use a URL and not an inline stylesheet.
-        // I'm doing a dumb compromise with data-URLs until I figure out how to move the files in a good way.
-        var razorPath = Path.ChangeExtension(csPath, null);
-        var cssPath = Path.GetExtension(razorPath) == ".razor" && File.Exists(razorPath) ? razorPath + ".css" : csPath + ".css";
-        return File.Exists(cssPath) ? $"data:text/css;base64,{Convert.ToBase64String(File.ReadAllBytes(cssPath))}" : null;
+            context.AddSource("additional-files.g.txt", string.Join("\n", webComponents.Select(x => $"{Path.GetFileName(x.FilePath)} {x.Name} {x.ContainingNamespace} {x.BaseType}")));
+        });
     }
 }
