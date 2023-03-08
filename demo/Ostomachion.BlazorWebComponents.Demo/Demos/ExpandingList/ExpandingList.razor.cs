c4ea67b7b24b9191ee788eb22bfdfe15fe6011fc
josh@hufford.io <josh@hufford.io>
2023-03-08 18:03:15 -0500

    Initial setup for expanding-list demo

---
+++ demo/Ostomachion.BlazorWebComponents.Demo/Demos/ExpandingList/ExpandingList.razor.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Components;
+using Ostomachion.BlazorWebComponents.Demo.Models;
+
+namespace Ostomachion.BlazorWebComponents.Demo.Demos.ExpandingList;
+
+[CustomElement("expanding-list")]
+public partial class ExpandingList
+{
+}
