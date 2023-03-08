0326227df1133b1c0d3f39930af2d3e68865ee3f
josh@hufford.io <josh@hufford.io>
2023-03-08 17:47:32 -0500

    Remove WebComponentBaseImpl.cs

--- src/Ostomachion.BlazorWebComponents/WebComponentBaseImpl.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections.Immutable;
-using System.ComponentModel;
-using System.Reflection;
-using System.Runtime.CompilerServices;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Rendering;
-using Microsoft.JSInterop;
-
-namespace Ostomachion.BlazorWebComponents;
