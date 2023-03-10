16e95db71f2acf5afa3ba445c39e5c4fab2e247c
josh@hufford.io <josh@hufford.io>
2023-03-09 22:50:47 -0500

    #2 Implement expanding-list demo

--- demo/Ostomachion.BlazorWebComponents.Demo/Demos/ExpandingList/ExpandingList.razor.cs
+++ demo/Ostomachion.BlazorWebComponents.Demo/Demos/ExpandingList/ExpandingList.razor.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace Ostomachion.BlazorWebComponents.Demo.Demos.ExpandingList;
 
 public partial class ExpandingList : CustomElementBase
 {
+
     [Parameter]
-    public RenderFragment ChildContent { get; set; } = default !;
+    public RenderFragment ChildContent { get; set; } = default!;
 }
