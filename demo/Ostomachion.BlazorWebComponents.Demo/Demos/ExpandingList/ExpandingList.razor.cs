c430b71dfb9282d7286016e5acfb22a50814fd70
josh@hufford.io <josh@hufford.io>
2023-03-10 14:16:00 -0500

    Fix initialization

--- demo/Ostomachion.BlazorWebComponents.Demo/Demos/ExpandingList/ExpandingList.razor.cs
+++ demo/Ostomachion.BlazorWebComponents.Demo/Demos/ExpandingList/ExpandingList.razor.cs
@@ -5,7 +5,20 @@ namespace Ostomachion.BlazorWebComponents.Demo.Demos.ExpandingList;
 
 public partial class ExpandingList : CustomElementBase
 {
+    [Inject]
+    public IJSRuntime JSRuntime { get; set; } = default!;
+
+    public IJSObjectReference? Module { get; set; }
 
     [Parameter]
     public RenderFragment ChildContent { get; set; } = default!;
+
+    protected override async Task OnAfterRenderAsync(bool firstRender)
+    {
+        if (firstRender)
+        {
+            Module = await JSRuntime.InvokeAsync<IJSObjectReference>("import", "./Demos/ExpandingList/ExpandingList.razor.js");
+            await Module.InvokeVoidAsync("initialize", Host);
+        }
+    }
 }
