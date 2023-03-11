da79471dc422bbaea8831a40f5d80bd6f45e2e30
josh@hufford.io <josh@hufford.io>
2023-03-11 00:57:24 -0500

    #16 Add XML doc comments to ServiceCollectionExtensions

--- src/Ostomachion.BlazorWebComponents/ServiceCollectionExtensions.cs
+++ src/Ostomachion.BlazorWebComponents/ServiceCollectionExtensions.cs
@@ -2,8 +2,18 @@
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Ostomachion.BlazorWebComponents;
+
+/// <summary>
+/// Extensions methods for <see cref="IServiceCollection"/> to help set up an app to use custom elements and web components.
+/// </summary>
 public static class ServiceCollectionExtensions
 {
+    /// <summary>
+    /// Sets up blazor web components on the specified <see cref="IServiceCollection"/>.
+    /// </summary>
+    /// <param name="services">The <see cref="IServiceCollection"/> to configure.</param>
+    /// <param name="rootComponents">The <see cref="RootComponentMappingCollection"/> for the app.</param>
+    /// <param name="configure">An action delegate to configure the provided <see cref="CustomElementRegistrar"/>.</param>
     public static void AddBlazorWebComponents(this IServiceCollection services, RootComponentMappingCollection rootComponents, Action<CustomElementRegistrar> configure)
     {
         rootComponents.Add<CustomElementRegistrarComponent>("head::after");
