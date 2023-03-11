8aeaf9ae8e7c26ff69cc4a4c9fee2fd1df4c30ba
josh@hufford.io <josh@hufford.io>
2023-03-10 20:09:45 -0500

    #4 Implement pop-up-info-box demo

--- demo/Ostomachion.BlazorWebComponents.Demo/Demos/PopUpInfoBox/PopupInfo.razor.cs
+++ demo/Ostomachion.BlazorWebComponents.Demo/Demos/PopUpInfoBox/PopupInfo.razor.cs
@@ -4,5 +4,11 @@ namespace Ostomachion.BlazorWebComponents.Demo.Demos.PopUpInfoBox;
 
 public partial class PopupInfo
 {
+    [Parameter]
+    [EditorRequired]
+    public string Image { get; set; } = default!;
 
+    [Parameter]
+    [EditorRequired]
+    public string Text { get; set; } = default!;
 }
