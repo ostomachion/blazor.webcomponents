4af120cc81f4878605d64215198a68762d0da821
josh@hufford.io <josh@hufford.io>
2023-03-11 00:45:04 -0500

    Add XML doc comments to ICustomElement

--- src/Ostomachion.BlazorWebComponents/ICustomElement.cs
+++ src/Ostomachion.BlazorWebComponents/ICustomElement.cs
@@ -5,6 +5,15 @@
 /// </summary>
 public interface ICustomElement
 {
+    /// <summary>
+    /// Gets or sets the identifier for the custom element.
+    /// </summary>
     public static abstract string? Identifier { get; set; }
-    public static abstract string LocalName { get; }
+
+    /// <summary>
+    /// <see langword="null"/> if the implementing class represents an autonomous custom element;
+    /// the name of the element being extended if the implementing class represents a customized
+    /// built-in element
+    /// </summary>
+    public static abstract string? LocalName { get; }
 }
