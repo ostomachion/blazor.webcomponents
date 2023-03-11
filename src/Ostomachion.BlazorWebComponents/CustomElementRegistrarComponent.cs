a06d95030ed3600d4939758ed902654eb5daf712
josh@hufford.io <josh@hufford.io>
2023-03-11 00:26:06 -0500

    Add XML doc comments to CustomElementRegistrarComponent.cs

--- src/Ostomachion.BlazorWebComponents/CustomElementRegistrarComponent.cs
+++ src/Ostomachion.BlazorWebComponents/CustomElementRegistrarComponent.cs
@@ -1,10 +1,18 @@
 ﻿using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System.Reflection;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Ostomachion.BlazorWebComponents;
 
-public class CustomElementRegistrarComponent : ComponentBase
+/// <summary>
+/// A special component that is responsible for defining custom elements that have been registered through
+/// <see cref="CustomElementRegistrar"/> with the JavaScript framework. This is added as a root component
+/// to <c>head::after</c> by <see cref="ServiceCollectionExtensions.AddBlazorWebComponents(IServiceCollection,
+/// RootComponentMappingCollection, Action{CustomElementRegistrar})"/>
+/// </summary>
+public sealed class CustomElementRegistrarComponent : ComponentBase
 {
     [Inject]
     private IJSRuntime JSRuntime { get; set; } = default!;
