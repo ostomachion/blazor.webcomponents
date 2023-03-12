d312b70059c140bc15d6d8831bfdc163938a6ed8
josh@hufford.io <josh@hufford.io>
2023-03-11 20:39:13 -0500

    #26 Add full validation for custom element identifiers

--- src/Ostomachion.BlazorWebComponents/CustomElementRegistrar.cs
+++ src/Ostomachion.BlazorWebComponents/CustomElementRegistrar.cs
@@ -1,11 +1,12 @@
 ﻿using System.Collections.Immutable;
 using System.Reflection;
 using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
 
 namespace Ostomachion.BlazorWebComponents;
 
 /// <inheritdoc cref="ICustomElementRegistrar"/>
-public class CustomElementRegistrar : ICustomElementRegistrar
+public partial class CustomElementRegistrar : ICustomElementRegistrar
 {
     private Dictionary<string, Type> _registrations { get; } = new();
 
@@ -29,6 +30,8 @@ public class CustomElementRegistrar : ICustomElementRegistrar
 
         // If we still don't have a name, construct one from the qualified name if possible.
         identifier ??= type.FullName?.ToLower().Replace('.', '-') ?? throw new NotSupportedException($"Cannot create an identifier for the component {type.FullName}.");
+
+        ThrowIfInvalidIdentifier(identifier);
 
         if (_registrations.TryGetValue(identifier, out var existingType))
         {
@@ -61,15 +64,25 @@ public class CustomElementRegistrar : ICustomElementRegistrar
         }
     }
 
-    /// <inheritdoc cref="ICustomElementRegistrar.RegisterAll"/>
-    public void RegisterAll() => ((ICustomElementRegistrar)this).RegisterAll();
+    //https://html.spec.whatwg.org/multipage/custom-elements.html#valid-custom-element-name
+    [GeneratedRegex(@"^[a-z]([-\.0-9_a-z\u00b7\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u037d\u037f-\u1fff\u200c-\u200d\u203f-\u2040\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd]|[\ud800-\udbff][\udc00-\udfff])*-([-\.0-9_a-z\u00b7\u00c0-\u00d6\u00d8-\u00f6\u00f8-\u037d\u037f-\u1fff\u200c-\u200d\u203f-\u2040\u2070-\u218f\u2c00-\u2fef\u3001-\ud7ff\uf900-\ufdcf\ufdf0-\ufffd]|[\ud800-\udbff][\udc00-\udfff])*\z", RegexOptions.ExplicitCapture)]
+    private static partial Regex PotentialCustomElementName();
+    private static readonly string[] ReservedNames = {
+        "annotation-xml",
+        "color-profile",
+        "font-face",
+        "font-face-src",
+        "font-face-uri",
+        "font-face-format",
+        "font-face-name",
+        "missing-glyph",
+    };
 
     private static void ThrowIfInvalidIdentifier(string identifier, [CallerArgumentExpression(nameof(identifier))] string? paramName = null)
     {
         ArgumentException.ThrowIfNullOrEmpty(identifier, paramName);
 
-        // TODO:
-        if (!identifier.Contains('-'))
+        if (!PotentialCustomElementName().IsMatch(identifier) || ReservedNames.Contains(identifier))
         {
             throw new ArgumentException($"{identifier} is not a valid custom element identifier.", paramName);
         }
